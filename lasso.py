5d3c674e2263a2bf755bd304e0e15c759c9b87d2
Neville Ingram <nlingram@wpi.edu>
2021-05-11 10:31:43 -0400

    Added plots for values of lambda

--- lasso.py
+++ lasso.py
@@ -1,6 +1,10 @@
 import pandas as p
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
+from sklearn import linear_model
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def lasso(file_name):
@@ -10,10 +14,43 @@ def lasso(file_name):
         X = df[["volatile acidity", "sulphates", "alcohol"]]
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-        clf = linear_model.Lasso(alpha=0.1)
-        clf.fit(X_train, y_train)
-        print("Model coefficients:", clf.coef_)
-        print("Model score on test set: %0.2f" % clf.score(X_test, y_test))
+        clf = linear_model.Lasso()
+
+        alphas = np.logspace(-6, 6, 200)
+        coefs = []
+        scores = []
+        mse = []
+        for a in alphas:
+            clf.set_params(alpha=a)
+            clf.fit(X_train, y_train)
+            yp = clf.predict(X_test)
+            coefs.append(clf.coef_)
+            mse.append(mean_squared_error(yp, y_test))
+            scores.append(clf.score(X_test, y_test))
+            print("%0.2f score with %0.2f alpha" % (clf.score(X_test, y_test), a))
+        # clf.fit(X_train, y_train)
+        plt.figure(figsize=(20, 6))
+        print("Best score on test set: %0.2f" % np.max(scores))
+
+        plt.subplot(121)
+        ax = plt.gca()
+        ax.plot(alphas, coefs)
+        ax.set_xscale('log')
+        plt.xlabel('alpha')
+        plt.ylabel('weights')
+        plt.title('Lasso coefficients as a function of the regularization')
+        plt.axis('tight')
+
+        plt.subplot(122)
+        ax = plt.gca()
+        ax.plot(alphas, mse)
+        ax.set_xscale('log')
+        plt.xlabel('alpha')
+        plt.ylabel('mse')
+        plt.title('Coefficient error as a function of the regularization')
+        plt.axis('tight')
+
+        plt.show()
 
 
 if __name__ == "__main__":
