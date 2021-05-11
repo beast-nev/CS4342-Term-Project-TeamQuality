1793a703a5fa8050b74c621052dc431cbf1ba8bc
Neville Ingram <nlingram@wpi.edu>
2021-05-11 11:58:43 -0400

    Testing different values for epsilon

--- svr.py
+++ svr.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import statsmodels.api as sm
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
@@ -13,12 +14,18 @@ def svr(file_name):
         X = df[["volatile acidity", "sulphates", "alcohol"]]
         X = sm.add_constant(X)
         X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-        regr = make_pipeline(StandardScaler(), SVR(C=1.0, epsilon=0.2))
-        regr.fit(X_train, y_train)
-        print('Accuracy of SVR on training set: {:.2f}'
-              .format(regr.score(X_train, y_train)))
-        print('Accuracy of SVR on test set: {:.2f}'
-              .format(regr.score(X_test, y_test)))
+        scores = []
+        epsilons = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+        for i in range(0, 10):
+            regr = make_pipeline(StandardScaler(), SVR(C=1.0, epsilon=epsilons[i]))
+            regr.fit(X_train, y_train)
+            # print('Accuracy of SVR on training set: %0.8f with epsilon: %0.2f'
+            #       % (regr.score(X_train, y_train), i))
+            print('Accuracy of SVR on test set: %0.8f with epsilon: %0.1f'
+                  % (regr.score(X_test, y_test), epsilons[i]))
+            scores.append(regr.score(X_test, y_test))
+
+        print(np.max(scores))
 
 
 if __name__ == "__main__":
